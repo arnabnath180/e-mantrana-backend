8cca9516e1696785e9bfc1b9d245f35e96c61ea9
arnabnath180 <arnabnath3030@gmail.com>
2023-02-19 18:34:39 +0530

    Random commit

--- src/main/java/com/example/emantranabackend/EMantranaBackendApplication.java
+++ src/main/java/com/example/emantranabackend/EMantranaBackendApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class EMantranaBackendApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(EMantranaBackendApplication.class, args);
+		System.out.println("ll");
 	}
 
 }
